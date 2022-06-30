ca490e0a7880fc0644abc40e7bf3208e6bc64b8e
dozie2022 <dandison.opara@yahoo.com>
2022-06-30 04:24:09 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n"); /*Print followed by new line*/
+return(0);
 }
