da58001774aff08d9d61a69d386fd0b8579f186e
dozie2022 <dandison.opara@yahoo.com>
2022-06-30 13:47:24 -0700

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/** 
+* main - Print a word,followed by a new line
+* Return: 0 if exited properly
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
