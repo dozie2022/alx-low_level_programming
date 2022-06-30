a33c97f880ccd4dd453a3fcb04b3be98f6c2115a
dozie2022 <dandison.opara@yahoo.com>
2022-06-30 14:16:26 -0700

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/* Print with new line */
-
+/**
+* main - prints with printf,followed by a new line
+* Return: 0 if exited properly
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
