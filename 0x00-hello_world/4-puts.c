93d40dc9255b35a2edaf1107611fe6f19395a892
dozie2022 <dandison.opara@yahoo.com>
2022-06-30 02:28:14 -0700

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
-#!/bin/bash
-#include <stdio.h>                                                                         
+#include <stdio.h>
 
-                                                                                           
-
-int main(void)                                                                             
-
-{                                                                                          
-
-puts("\"Programming is like building a multilingual puzzle, followed by a new line.");     
-
-return (0);                                                                                
-
-}                                          
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+return (0);
+}
