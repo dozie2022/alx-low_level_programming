4633f6658092a1ee73151ea73a3a9d81599d016b
dozie2022 <dandison.opara@yahoo.com>
2022-06-30 02:41:48 -0700

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
