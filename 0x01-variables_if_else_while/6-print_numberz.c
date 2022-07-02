7da786c6304d1be16abe0777ac6757f4d5c3e3b0
dozie2022 <dandison.opara@yahoo.com>
2022-07-02 03:49:52 -0700

    Base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - program that prints all single digit numbers of base 10
+* starting from 0
 * Return: Always 0
 */
 int main(void)
 {
 int i;
-for (i = 48; i <= 57; i++)
+for (i = 48; i = 57; i++)
 {
 putchar (i);
 }
